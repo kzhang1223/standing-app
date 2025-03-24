ebea76e85c1088a9d0200ade9db204338037a91d
kathy <kzhang1223@gmail.com>
2025-03-23 21:48:24 -0700

    added start and pause button functionality

--- src/render.js
+++ src/render.js
@@ -2,7 +2,12 @@ const Hours = document.getElementById("hours");
 const Minutes = document.getElementById("minutes");
 const Seconds = document.getElementById("seconds");
 
+var startBtnClicked = false;
+var startTimerId;
+
+// initializes and sets timer up
 var currentTime = 60 * 60;
+var currentTimeTotal = 60 * 60;
 
 function timer () {
 
@@ -23,4 +28,20 @@ function timer () {
     currentTime--;
 }
 
-setInterval(timer, 1000);
+// start button functionality
+function startTimer () {
+    if (!startBtnClicked) {
+        startTimerId = setInterval(timer, 1000);
+        console.log(startTimerId);
+        startBtnClicked = true;
+    }
+}
+
+// pause button functionality
+function stopTimer () {
+    console.log("timer should stop");
+    console.log(startTimerId);
+    clearInterval(startTimerId);
+    startBtnClicked = false;
+}
+
